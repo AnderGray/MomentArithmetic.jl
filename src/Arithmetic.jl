5be65bcade46081cc1384356889cc31106a89c51
Ander Gray <ander.gray@liverpool.ac.uk>
2020-09-22 14:16:22 +0100

    adds rowe & rowevar

--- src/Arithmetic.jl
+++ src/Arithmetic.jl
@@ -13,28 +13,135 @@
 ######
 
 
+import Base: exp, sqrt, ^, log, ln
+
+function env(x, y)
+    MIN = min(left(x), left(y));
+    MAX = max(right(x), right(y))
+    return interval(MIN, MAX);
+end
+
+left(x :: AbstractMoment) = x.range.lo
+right(x :: AbstractMoment) = x.range.hi
+
+function rowe(x :: AbstractMoment, t :: Function)
+
+    EX = x.mean; VX = x.var;
+    LX = left(x); GX = right(x); 
+
+    if typeof(EX) <: Interval;  throw(ArgumentError("Rowe mean must have precise moment inputs")); end    
+    if typeof(VX) <: Interval;  throw(ArgumentError("Rowe mean must have precise moment inputs")); end    
+
+    P = 1 / (1 + (EX - LX)^2/ VX)
+    Q = 1 / (1 + (GX - EX)^2/ VX)
+
+    MIN = P * t(LX) + (1 - P) * t(EX + VX/(EX - LX))
+    MAX = Q * t(GX) + (1 - Q) * t(EX + VX/(EX - GX))
+
+    return env(MIN, MAX)
+
+end
+
+function rowevar(x :: AbstractMoment, t :: Function, invT :: Function)
+    
+    EX = x.mean; VX = x.var;
+    LX = left(x); GX = right(x); 
+
+    if typeof(EX) <: Interval;  throw(ArgumentError("Rowe var must have precise moment inputs")); end    
+    if typeof(VX) <: Interval;  throw(ArgumentError("Rowe var must have precise moment inputs")); end    
+
+    v = invT(rowe(x, t))
+
+    Lv = left(v); Gv = right(v);
+
+    MIN = ((t(Lv) - t(LX))/(Lv - LX))^2 * (VX + (Lv - EX)^2)
+    MAX = ((t(Gv) - t(GX))/(Gv - GX))^2 * (VX + (Gv - EX)^2)
+    
+    return env(MIN, MAX)
+
+end
+
+
 ### 
 #   Univariate transformations
 ###
 -(x :: AbstractMoment) = Moments(-x.mean, x.var, -x.range)
 
 function reciprocal(x :: AbstractMoment)
-    throw(ArgumentError("rowe reciprocate must be implemented")) 
+    f(x) = 1/x
+    invF = *
+
+    MEAN = rowe(x, f)
+    VAR = rowevar(x, f, invF)
+    RANGE = f(x.range)
+
+    return Moments(MEAN, VAR, RANGE)
+
 end
+
+reciprocal(x :: Real) = 1/x
 
 function exp(x :: AbstractMoment)
-    throw(ArgumentError("rowe exponentiate must be implemented")) 
+
+    MEAN = rowe(x, exp)
+    VAR = rowevar(x, exp, log)
+    RANGE = f(x.range)
+
+    return Moments(MEAN, VAR, RANGE)
+
 end
 
-^(x :: AbstractMoment) = throw(ArgumentError("rowe exponentiate must be implemented")) 
+#^(x :: AbstractMoment) = throw(ArgumentError("rowe exponentiate must be implemented")) 
+
+function ^(x :: AbstractMoment, a :: Real)
+
+    if !(a == 2); throw(ArgumentError("Only rowe squared works so far")); end
+
+    f(x) = x^2
+    invF(x) = sqrt(x)
+
+    MEAN = rowe(x, f) 
+    VAR = rowevar(x, f, invF)
+    RANGE = f(x.range)
+    
+    return Moments(MEAN, VAR, RANGE)
+end
+
+function sqrt(x :: AbstractMoment)
+
+    f(x) = sqrt(x)
+    invF(x) = x^2
+
+    MEAN = rowe(x, f) 
+    VAR = rowevar(x, f, invF)
+    RANGE = f(x.range)
+    
+    return Moments(MEAN, VAR, RANGE)
+end
 
 function ln(x :: AbstractMoment)
-    throw(ArgumentError("rowe natural log must be implemented")) 
+    
+    f(x) = log(x)
+    invF(x) = exp(x)
+
+    MEAN = rowe(x, f)
+    VAR = rowevar(x, f, invF)
+    RANGE = f(x.range)
+
+    return Moments(MEAN, VAR, RANGE)
 end
 
-function log(x :: AbstractMoment, base :: Real)
+function log(x :: AbstractMoment, base = ℯ :: Real)
     if base == ℯ; ln(x);end
-    throw(ArgumentError("rowe log must be implemented")) 
+
+    f(x) = log(x, base)
+    invF(x) = base^x
+
+    MEAN = rowe(x, f) 
+    VAR = rowevar(x, f, invF)
+    RANGE = f(x.range)
+    
+    return Moments(MEAN, VAR, RANGE)
 end
 
 
@@ -49,7 +156,7 @@ end
 *(x :: AbstractMoment, y :: Real) = Moments(x.mean * y, x.var * y^2, x.range * y)
 *(x :: Real, y::AbstractMoment) = y * x;
 /(x :: AbstractMoment, y :: Real) = x * (1/y);
-/(x :: Real, y::AbstractMoment) = y * reciprocal(x);
+/(x :: Real, y :: AbstractMoment) = x * reciprocal(y);
 
 
 ###
@@ -66,7 +173,7 @@ function sumIndep(x :: AbstractMoment, y ::AbstractMoment)
     zRange = x.range + y.range;
 
     # Add dependent variables to dict
-    return Moments(zMean,zVar,zRange);
+    return Moments(zMean, zVar, zRange);
 end
 
 function subIndep(x :: AbstractMoment, y ::AbstractMoment)
@@ -76,7 +183,7 @@ function subIndep(x :: AbstractMoment, y ::AbstractMoment)
     zRange = x.range - y.range;
 
     # Add dependent variables to dict
-    return Moments(zMean,zVar,zRange);
+    return Moments(zMean, zVar, zRange);
 end
 
 function multIndep(x :: AbstractMoment, y ::AbstractMoment)
@@ -86,7 +193,7 @@ function multIndep(x :: AbstractMoment, y ::AbstractMoment)
     zRange = x.range * y.range;
 
     # Add dependent variables to dict
-    return Moments(zMean,zVar,zRange);
+    return Moments(zMean, zVar, zRange);
 
 end
 
@@ -95,7 +202,7 @@ function divIndep(x :: AbstractMoment, y ::AbstractMoment)
 end
 
 +(x :: AbstractMoment, y ::AbstractMoment) = sumIndep(x,y);
--(x :: AbstractMoment, y ::AbstractMoment) = subIndep(x.y);
+-(x :: AbstractMoment, y ::AbstractMoment) = subIndep(x,y);
 *(x :: AbstractMoment, y ::AbstractMoment) = multIndep(x,y);
 /(x :: AbstractMoment, y ::AbstractMoment) = divIndep(x,y);
 
--- src/Moments.jl
+++ src/Moments.jl
@@ -44,9 +44,9 @@ import Base.^
 
 import Base.issubset, Base.intersect
 
-using IntervalArithmetic
+using IntervalArithmetic, ProbabilityBoundsAnalysis
 
-abstract type AbstractMoment <: Real end
+abstract type AbstractMoment end
 
 include("Arithmetic.jl")
 include("Utils.jl")
@@ -66,44 +66,9 @@ mutable struct Moments <: AbstractMoment
     #CorreltedVars :: Union{Missing, Array{UInt64,1}} # For defining which variables this one is correlted too, for dependency tracking
     #Unit ::  Symbol   # Will add this later
 
-    function Moments( mean = missing,  var = missing, range :: Interval = missing)
+    function Moments( mean = missing,  var = missing, range = missing)
 
-        if ismissing(mean);     mean    = interval(-Inf,Inf);   end
-        if ismissing(var);      var     = interval(0,Inf);      end
-        if ismissing(range);    range   = interval(-Inf,Inf);   end
-
-        # mean constraint
-        if (mean ∩ range == ∅); throw(ArgumentError("Provided information not valid. Mean ∩ Range = ∅.\n       $mean ∩ $range = ∅")); end
-        if !( mean ⊆ range); 
-            ml = max(left(mean), left(range));
-            mh = min(right(mean), right(range));
-            mean = interval(ml,mh);
-        end
-
-        # Var constraint
-        maxVar = interval(0,Inf)
-        if isfinite(range.hi) && isfinite(range.lo)
-            maxRange = range.hi; minRange = range.lo; 
-            meanMax = right(mean); meanMin = left(mean);
-
-            v1 = (maxRange-minRange)*(maxRange-meanMin)-(maxRange-meanMin)*(maxRange-meanMin);
-            v2 = (maxRange-minRange)*(maxRange-meanMax)-(maxRange-meanMax)*(maxRange-meanMax);
-            v3 = 0; mid = (range.hi-range.lo)/2;
-
-            if (mid ∈ mean); v3 =  (maxRange-minRange)*(maxRange-mid)-(maxRange-mid)*(maxRange-mid); end
-            
-            vh = max(v1, v2, v3); vl = 0;    
-            maxVar = interval(vl,vh);
-        end 
-
-        println(maxVar)
-
-        if (var ∩ maxVar == ∅); throw(ArgumentError("Provided information not valid. Variance ∩ VarBounds = ∅.\n       $var ∩ $maxVar = ∅")); end
-        if !(var ⊆ maxVar)
-            vl = max(left(var), left(maxVar));
-            vh = min(right(var), right(maxVar));
-            var = interval(vl, vh)
-        end
+        mean, var, range = make_consistent(mean, var, range)
 
         return new(mean, var, range)
 
@@ -116,6 +81,51 @@ left(x :: Interval) = x.lo;         left(x :: Real) = x;
 right(x :: Interval) = x.hi;        right(x :: Real) = x
 
 
+function make_consistent(mean = missing,  var = missing, range = missing)
+
+
+    if ismissing(mean);     mean    = interval(-Inf,Inf);   end
+    if ismissing(var);      var     = interval(0,Inf);      end
+    if ismissing(range);    range   = interval(-Inf,Inf);   end
+
+    # mean constraint
+    if (mean ∩ range == ∅); throw(ArgumentError("Provided information not valid. Mean ∩ Range = ∅.\n       $mean ∩ $range = ∅")); end
+    if !( mean ⊆ range); 
+        ml = max(left(mean), left(range));
+        mh = min(right(mean), right(range));
+        mean = interval(ml,mh);
+    end
+
+    # Var constraint
+    maxVar = interval(0,Inf)
+    if isfinite(range.hi) && isfinite(range.lo)
+        maxRange = range.hi; minRange = range.lo; 
+        meanMax = right(mean); meanMin = left(mean);
+
+        v1 = (maxRange-minRange)*(maxRange-meanMin)-(maxRange-meanMin)*(maxRange-meanMin);
+        v2 = (maxRange-minRange)*(maxRange-meanMax)-(maxRange-meanMax)*(maxRange-meanMax);
+        v3 = 0; mid = (range.hi + range.lo)/2;
+
+        if (mid ∈ mean); v3 =  (maxRange-minRange)*(maxRange-mid)-(maxRange-mid)*(maxRange-mid); end
+        
+        #println("v1: $v1");println("v2: $v2");println("v3: $v3")
+
+        vh = max(v1, v2, v3); vl = 0;    
+        maxVar = interval(vl,vh);
+    end 
+
+    if (var ∩ maxVar == ∅); throw(ArgumentError("Provided information not valid. Variance ∩ VarBounds = ∅.\n       $var ∩ $maxVar = ∅")); end
+    if !(var ⊆ maxVar)
+        vl = max(left(var), left(maxVar));
+        vh = min(right(var), right(maxVar));
+        var = interval(vl, vh)
+    end
+
+    return mean, var, range
+
+end
+
+function 
 
 
 function Base.show(io::IO, z::Moments)
